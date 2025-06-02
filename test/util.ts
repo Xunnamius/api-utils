36d8d3724977e96addf4d954071fcb466cfe3b2c
Xunnamius (Romulus) <Xunnamius@users.noreply.github.com>
2025-06-01 22:11:18 -0700

    test(util): inline package from mongo-utils

--- test/util.ts
+++ test/util.ts
@@ -3,6 +3,8 @@
  ** exported by @-xun/jest; these can be imported using the testversal aliases.
  */
 
+import type { WithId } from 'mongodb';
+
 // ? These will always come from @-xun/symbiote and @-xun/jest (transitively)
 // {@symbiote/notInvalid
 //   - @-xun/jest
@@ -15,3 +17,18 @@
 // }
 
 export * from '@-xun/jest';
+
+/**
+ * A sort predicate meant to be used with {@link Array.prototype.sort} when
+ * ordering items by their `ObjectId`. In the majority of cases, the result
+ * should be the same as what MongoDb would return with `{ sort: { _id: 1 }}`).
+ */
+export function objectIdPseudoSortPredicate(order: 'ascending' | 'descending') {
+  return (a: WithId<unknown>, b: WithId<unknown>) => {
+    const [first, second] = order === 'ascending' ? [a, b] : [b, a];
+    return (
+      Number.parseInt(first._id.toString(), 16) -
+      Number.parseInt(second._id.toString(), 16)
+    );
+  };
+}
