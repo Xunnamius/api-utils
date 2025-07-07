6e6f457f81ef4777a4dfe56367f8c149c634821e
Xunnamius (Romulus) <Xunnamius@users.noreply.github.com>
2025-07-06 18:21:05 -0700

    fix(packages/api-strategy): re-export @-xun/api-error

--- packages/api-strategy/src/error.ts
+++ packages/api-strategy/src/error.ts
@@ -33,13 +33,4 @@ export const ErrorMessage = {
   }
 };
 
-// TODO: replace/augment with @-xun/error
-
-export class ApiError extends Error {}
-export class SanityError extends ApiError {}
-export class ClientValidationError extends ApiError {}
-export class ServerValidationError extends ApiError {}
-export class AuthError extends ApiError {}
-export class ForbiddenError extends ApiError {}
-export class NotFoundError extends ApiError {}
-export class NotImplementedError extends ApiError {}
+export * from '@-xun/api-error';
