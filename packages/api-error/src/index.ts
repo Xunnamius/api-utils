717072591c11a3f649cff8ef8ca3b37afce2c8c2
Xunnamius (Romulus) <Xunnamius@users.noreply.github.com>
2025-07-10 03:29:53 -0700

    fix(packages/api-error): explicitly export types as `Error`s for the benefit of linters

--- packages/api-error/src/index.ts
+++ packages/api-error/src/index.ts
@@ -2,83 +2,82 @@ import { makeNamedError } from '@-xun/error';
 
 export { isANamedErrorClass, isANamedErrorInstance } from '@-xun/error';
 
-const { ApiError } = makeNamedError(class ApiError extends Error {}, 'ApiError');
+export const { ApiError } = makeNamedError(class ApiError extends Error {}, 'ApiError');
 
-const { SanityError } = makeNamedError(
+export const { SanityError } = makeNamedError(
   class SanityError extends ApiError {},
   'SanityError'
 );
 
-const { ClientValidationError } = makeNamedError(
+export const { ClientValidationError } = makeNamedError(
   class ClientValidationError extends ApiError {},
   'ClientValidationError'
 );
 
-const { ServerValidationError } = makeNamedError(
+export const { ServerValidationError } = makeNamedError(
   class ServerValidationError extends ApiError {},
   'ServerValidationError'
 );
 
-const { AuthError } = makeNamedError(class AuthError extends ApiError {}, 'AuthError');
+export const { AuthError } = makeNamedError(
+  class AuthError extends ApiError {},
+  'AuthError'
+);
 
-const { ForbiddenError } = makeNamedError(
+export const { ForbiddenError } = makeNamedError(
   class ForbiddenError extends ApiError {},
   'ForbiddenError'
 );
 
-const { NotFoundError } = makeNamedError(
+export const { NotFoundError } = makeNamedError(
   class NotFoundError extends ApiError {},
   'NotFoundError'
 );
 
-const { NotImplementedError } = makeNamedError(
+export const { NotImplementedError } = makeNamedError(
   class extends ApiError {},
   'NotImplementedError'
 );
 
+/**
+ * Generic base API-related error (HTTP 500).
+ *
+ * This class is the ancestor of all error types exported by
+ * `@-xun/api-error`.
+ */
 export type ApiError = InstanceType<typeof ApiError>;
-export type AuthError = InstanceType<typeof AuthError>;
-export type ClientValidationError = InstanceType<typeof ClientValidationError>;
-export type ForbiddenError = InstanceType<typeof ForbiddenError>;
-export type NotFoundError = InstanceType<typeof NotFoundError>;
-export type NotImplementedError = InstanceType<typeof NotImplementedError>;
-export type SanityError = InstanceType<typeof SanityError>;
-export type ServerValidationError = InstanceType<typeof ServerValidationError>;
 
-export {
-  /**
-   * Generic base API-related error (HTTP 500).
-   *
-   * This class is the ancestor of all error types exported by
-   * `@-xun/api-error`.
-   */
-  ApiError,
-  /**
-   * An authentication or generic authorization issue (HTTP 401).
-   */
-  AuthError,
-  /**
-   * A client-side validation issue (HTTP 400).
-   */
-  ClientValidationError,
-  /**
-   * A resource authorization issue (HTTP 403).
-   */
-  ForbiddenError,
-  /**
-   * A resource existence issue (HTTP 404).
-   */
-  NotFoundError,
-  /**
-   * An issue with unimplemented functionality (HTTP 501).
-   */
-  NotImplementedError,
-  /**
-   * An impossible issue (HTTP 500).
-   */
-  SanityError,
-  /**
-   * A server-side validation issue (HTTP 500).
-   */
-  ServerValidationError
-};
+/**
+ * An authentication or generic authorization issue (HTTP 401).
+ */
+export type AuthError = InstanceType<typeof AuthError>;
+
+/**
+ * A client-side validation issue (HTTP 400).
+ */
+export type ClientValidationError = InstanceType<typeof ClientValidationError>;
+
+/**
+ * A resource authorization issue (HTTP 403).
+ */
+export type ForbiddenError = InstanceType<typeof ForbiddenError>;
+
+/**
+ * A resource existence issue (HTTP 404).
+ */
+export type NotFoundError = InstanceType<typeof NotFoundError>;
+
+/**
+ * An issue with unimplemented functionality (HTTP 501).
+ */
+export type NotImplementedError = InstanceType<typeof NotImplementedError>;
+
+/**
+ * An impossible issue (HTTP 500).
+ */
+export type SanityError = InstanceType<typeof SanityError>;
+
+/**
+ * A server-side validation issue (HTTP 500).
+ */
+export type ServerValidationError = InstanceType<typeof ServerValidationError>;
