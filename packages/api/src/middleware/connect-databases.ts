30c5a3a3a39a239c61fa2d5f9cd0bc2c557b6947
Xunnamius (Romulus) <Xunnamius@users.noreply.github.com>
2025-07-12 20:16:43 -0700

    feat(packages/api): add new "connect-databases" middleware

---
+++ packages/api/src/middleware/connect-databases.ts
@@ -0,0 +1,107 @@
+import { getEnv } from '@-xun/env';
+import { getDb, setSchemaConfig } from '@-xun/mongo-schema';
+
+import { globalDebugLogger } from 'universe+api:constant.ts';
+import { ErrorMessage } from 'universe+api:error.ts';
+
+import type { DbSchema } from '@-xun/mongo-schema';
+import type { DummyData } from '@-xun/mongo-test';
+import type { EmptyObject } from 'type-fest';
+
+import type {
+  ExportedMiddleware,
+  MiddlewareContext,
+  ModernOrLegacyMiddleware
+} from 'universe+api:types.ts';
+
+const debug = globalDebugLogger.extend('connect-databases');
+
+export type Options = {
+  /**
+   * Options specific to the connect-databases middleware.
+   */
+  database: {
+    /**
+     * A list of database names or aliases that will be initialized and hydrated
+     * with dummy data when NODE_ENV starts with "development" and the
+     * API_HYDRATE_DB environment variable is defined.
+     *
+     * Usually, "root" should always been listed here. Similarly, the similarly
+     * well-known alias "app" should also usually be listed here.
+     *
+     * @default ['root', 'app']
+     */
+    hydrationTargets?: string[];
+    /**
+     * Passed to {@link setSchemaConfig} at the appropriate point.
+     */
+    schema: () => DbSchema;
+    /**
+     * Passed to {@link setDummyData} at the appropriate point.
+     */
+    data: () => DummyData;
+  };
+};
+
+export type Context = EmptyObject;
+
+/**
+ * Sets the database schema(s) if the NODE_ENV environment variable starts with
+ * "production" or "development". Additionally hydrates the database(s) with
+ * dummy data if NODE_ENV starts with "development" and the API_HYDRATE_DB
+ * environment variable is defined.
+ *
+ * This middleware will not pull in any non-production dependencies when used in
+ * a production environment.
+ */
+export function makeMiddleware() {
+  return async function (_reqOrRequest, resOrModernContext, maybeLegacyContext) {
+    const isInLegacyMode = !!maybeLegacyContext;
+    debug('entered middleware runtime (mode: %O)', isInLegacyMode ? 'LEGACY' : 'MODERN');
+
+    const {
+      options: { database }
+    } = (isInLegacyMode ? maybeLegacyContext : resOrModernContext) as MiddlewareContext<
+      Options,
+      Context,
+      ModernOrLegacyMiddleware<Options, Context>
+    >;
+
+    const isProduction = getEnv().NODE_ENV.startsWith('production');
+    const isDevelopment = getEnv().NODE_ENV.startsWith('development');
+
+    if (isProduction || isDevelopment) {
+      setSchemaConfig(database.schema);
+      debug('set global database schema');
+
+      if (isDevelopment && getEnv().API_HYDRATE_DB) {
+        const { hydrateDbWithDummyData, setDummyData } = require(
+          // ? This expression stops webpack/turbopack from bundling the thing
+          '@-xun/mongo-test'.toString()
+        ) as typeof import('@-xun/mongo-test');
+
+        debug('executing api db initialize-and-hydrate directive');
+
+        setDummyData(database.data);
+
+        const { hydrationTargets = ['root', 'app'] } = database;
+
+        await Promise.all(hydrationTargets.map((name) => getDb({ name })));
+
+        debug('dbs initialized successfully: %O', hydrationTargets);
+
+        await Promise.all(
+          hydrationTargets.map((name) => hydrateDbWithDummyData({ name }))
+        );
+
+        debug('dbs hydrated successfully: %O', hydrationTargets);
+
+        throw new Error(ErrorMessage.ApiHydrateDbSucceeded());
+      } else {
+        debug('skipped db initialize-and-hydrate directive');
+      }
+    } else {
+      debug('skipped setting global database schema');
+    }
+  } satisfies ExportedMiddleware<Options, Context>;
+}
---
+++ packages/api/test/middleware/unit-connect-databases.test.ts
@@ -0,0 +1,357 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { getDb } from '@-xun/mongo-schema';
+import { setupMemoryServerOverride } from '@-xun/mongo-test';
+import { testApiHandler } from 'next-test-api-route-handler';
+
+import { withMiddleware } from 'universe+api';
+import { getCommonDummyData } from 'universe+api-strategy:mongo/dummy.ts';
+import { getCommonSchemaConfig } from 'universe+api-strategy:mongo/index.ts';
+import { ErrorMessage } from 'universe+api:error.ts';
+import { makeMiddleware } from 'universe+api:middleware/connect-databases.ts';
+
+import {
+  legacyNoopHandler,
+  modernNoopHandler,
+  useMockDateNow,
+  withLegacyConfig,
+  withMockedEnv
+} from 'testverse:util.ts';
+
+import type { DbSchema } from '@-xun/mongo-schema';
+import type { DummyData } from '@-xun/mongo-test';
+import type { ModernMiddlewareContext, NextApiResponseLike } from 'universe+api';
+import type { Context, Options } from 'universe+api:middleware/connect-databases.ts';
+
+useMockDateNow();
+const { initializeMemoryServerOverride, killMemoryServerOverride } =
+  setupMemoryServerOverride({
+    schema: { aliases: {}, databases: {} },
+    data: {},
+    defer: 'without-initialization'
+  });
+
+const fakeSchema: DbSchema = {
+  databases: { 'fake-db': { collections: [{ name: 'fake-collection' }] } },
+  aliases: {}
+};
+
+const fakeData: DummyData = {
+  'fake-db': { _generatedAt: Date.now(), 'fake-collection': [{ item: 1 }, { item: 2 }] }
+};
+
+beforeEach(async () => {
+  await initializeMemoryServerOverride();
+});
+
+afterEach(async () => {
+  await killMemoryServerOverride();
+});
+
+describe('<legacy mode>', () => {
+  it('sets db schema specifically when in production or development mode but does not hydrate database', async () => {
+    expect.hasAssertions();
+
+    await withMockedEnv(
+      async () => {
+        await testApiHandler({
+          rejectOnHandlerError: true,
+          pagesHandler: withLegacyConfig(
+            withMiddleware<Options, Context>(legacyNoopHandler, {
+              descriptor: '/fake',
+              use: [makeMiddleware()],
+              options: {
+                legacyMode: true,
+                database: {
+                  schema: () => fakeSchema,
+                  data: () => fakeData
+                }
+              }
+            })
+          ),
+          test: async ({ fetch }) => {
+            await expect(fetch().then((r) => r.status)).resolves.toBe(200);
+
+            const db = await getDb({ name: 'fake-db' });
+
+            await expect(db.listCollections().toArray()).resolves.toStrictEqual([
+              expect.objectContaining({ name: 'fake-collection' })
+            ]);
+
+            const collection = db.collection('fake-collection');
+
+            await expect(collection.find().toArray()).resolves.toStrictEqual([]);
+          }
+        });
+      },
+      { NODE_ENV: 'production', MONGODB_URI: 'ignored://' }
+    );
+  });
+
+  it('sets db schema and hydrates db when given API_HYDRATE_DB specifically in development mode', async () => {
+    expect.hasAssertions();
+
+    await withMockedEnv(
+      async () => {
+        await testApiHandler({
+          rejectOnHandlerError: true,
+          pagesHandler: withLegacyConfig(
+            withMiddleware<Options, Context>(legacyNoopHandler, {
+              descriptor: '/fake',
+              use: [makeMiddleware()],
+              useOnError: [
+                (_req, res, ctx) => {
+                  const {
+                    runtime: { error }
+                  } = ctx!;
+
+                  expect(error).toMatchObject({
+                    message: ErrorMessage.ApiHydrateDbSucceeded()
+                  });
+
+                  (res as NextApiResponseLike).status(200).send({});
+                }
+              ],
+              options: {
+                legacyMode: true,
+                database: {
+                  hydrationTargets: ['root', 'fake-db'],
+                  schema: () => getCommonSchemaConfig(fakeSchema),
+                  data: () => getCommonDummyData(fakeData)
+                }
+              }
+            })
+          ),
+          test: async ({ fetch }) => {
+            await expect(fetch().then((r) => r.status)).resolves.toBe(200);
+
+            const db = await getDb({ name: 'fake-db' });
+
+            await expect(db.listCollections().toArray()).resolves.toStrictEqual([
+              expect.objectContaining({ name: 'fake-collection' })
+            ]);
+
+            const collection = db.collection('fake-collection');
+
+            await expect(collection.find().toArray()).resolves.toStrictEqual([
+              expect.objectContaining({ _id: expect.anything(), item: 1 }),
+              expect.objectContaining({ _id: expect.anything(), item: 2 })
+            ]);
+          }
+        });
+      },
+      { NODE_ENV: 'development', API_HYDRATE_DB: 'true', MONGODB_URI: 'ignored://' }
+    );
+  });
+
+  it('defaults hydrationTargets to "root" and "app"', async () => {
+    expect.hasAssertions();
+
+    await withMockedEnv(
+      async () => {
+        await testApiHandler({
+          rejectOnHandlerError: true,
+          pagesHandler: withLegacyConfig(
+            withMiddleware<Options, Context>(legacyNoopHandler, {
+              descriptor: '/fake',
+              use: [makeMiddleware()],
+              useOnError: [
+                (_req, res, ctx) => {
+                  const {
+                    runtime: { error }
+                  } = ctx!;
+
+                  expect(error).toMatchObject({
+                    message: ErrorMessage.ApiHydrateDbSucceeded()
+                  });
+
+                  (res as NextApiResponseLike).status(200).send({});
+                }
+              ],
+              options: {
+                legacyMode: true,
+                database: {
+                  schema: () =>
+                    getCommonSchemaConfig({
+                      ...fakeSchema,
+                      aliases: { app: 'fake-db' }
+                    }),
+                  data: () => getCommonDummyData(fakeData)
+                }
+              }
+            })
+          ),
+          test: async ({ fetch }) => {
+            await expect(fetch().then((r) => r.status)).resolves.toBe(200);
+
+            const db = await getDb({ name: 'fake-db' });
+
+            await expect(db.listCollections().toArray()).resolves.toStrictEqual([
+              expect.objectContaining({ name: 'fake-collection' })
+            ]);
+
+            const collection = db.collection('fake-collection');
+
+            await expect(collection.find().toArray()).resolves.toStrictEqual([
+              expect.objectContaining({ _id: expect.anything(), item: 1 }),
+              expect.objectContaining({ _id: expect.anything(), item: 2 })
+            ]);
+          }
+        });
+      },
+      { NODE_ENV: 'development', API_HYDRATE_DB: 'true', MONGODB_URI: 'ignored://' }
+    );
+  });
+});
+
+describe('<modern mode>', () => {
+  it('sets db schema specifically when in production or development mode but does not hydrate database', async () => {
+    expect.hasAssertions();
+
+    await withMockedEnv(
+      async () => {
+        await testApiHandler({
+          rejectOnHandlerError: true,
+          appHandler: withMiddleware<Options, Context>(modernNoopHandler, {
+            descriptor: '/fake',
+            use: [makeMiddleware()],
+            options: {
+              database: {
+                schema: () => fakeSchema,
+                data: () => fakeData
+              }
+            }
+          }),
+          test: async ({ fetch }) => {
+            await expect(fetch().then((r) => r.status)).resolves.toBe(200);
+
+            const db = await getDb({ name: 'fake-db' });
+
+            await expect(db.listCollections().toArray()).resolves.toStrictEqual([
+              expect.objectContaining({ name: 'fake-collection' })
+            ]);
+
+            const collection = db.collection('fake-collection');
+
+            await expect(collection.find().toArray()).resolves.toStrictEqual([]);
+          }
+        });
+      },
+      { NODE_ENV: 'production', MONGODB_URI: 'ignored://' }
+    );
+  });
+
+  it('sets db schema and hydrates db when given API_HYDRATE_DB specifically in development mode', async () => {
+    expect.hasAssertions();
+
+    await withMockedEnv(
+      async () => {
+        await testApiHandler({
+          rejectOnHandlerError: true,
+          appHandler: withMiddleware<Options, Context>(modernNoopHandler, {
+            descriptor: '/fake',
+            use: [makeMiddleware()],
+            useOnError: [
+              (_, ctx_) => {
+                const ctx = ctx_ as ModernMiddlewareContext<any, any>;
+                const {
+                  runtime: { error }
+                } = ctx;
+
+                expect(error).toMatchObject({
+                  message: ErrorMessage.ApiHydrateDbSucceeded()
+                });
+
+                // @ts-expect-error: reset
+                ctx.runtime.response = null;
+                return Response.json({}, { status: 200 });
+              }
+            ],
+            options: {
+              database: {
+                hydrationTargets: ['root', 'fake-db'],
+                schema: () => getCommonSchemaConfig(fakeSchema),
+                data: () => getCommonDummyData(fakeData)
+              }
+            }
+          }),
+          test: async ({ fetch }) => {
+            await expect(fetch().then((r) => r.status)).resolves.toBe(200);
+
+            const db = await getDb({ name: 'fake-db' });
+
+            await expect(db.listCollections().toArray()).resolves.toStrictEqual([
+              expect.objectContaining({ name: 'fake-collection' })
+            ]);
+
+            const collection = db.collection('fake-collection');
+
+            await expect(collection.find().toArray()).resolves.toStrictEqual([
+              expect.objectContaining({ _id: expect.anything(), item: 1 }),
+              expect.objectContaining({ _id: expect.anything(), item: 2 })
+            ]);
+          }
+        });
+      },
+      { NODE_ENV: 'development', API_HYDRATE_DB: 'true', MONGODB_URI: 'ignored://' }
+    );
+  });
+
+  it('defaults hydrationTargets to "root" and "app"', async () => {
+    expect.hasAssertions();
+
+    await withMockedEnv(
+      async () => {
+        await testApiHandler({
+          rejectOnHandlerError: true,
+          appHandler: withMiddleware<Options, Context>(modernNoopHandler, {
+            descriptor: '/fake',
+            use: [makeMiddleware()],
+            useOnError: [
+              (_, ctx_) => {
+                const ctx = ctx_ as ModernMiddlewareContext<any, any>;
+                const {
+                  runtime: { error }
+                } = ctx;
+
+                expect(error).toMatchObject({
+                  message: ErrorMessage.ApiHydrateDbSucceeded()
+                });
+
+                // @ts-expect-error: reset
+                ctx.runtime.response = null;
+                return Response.json({}, { status: 200 });
+              }
+            ],
+            options: {
+              database: {
+                schema: () =>
+                  getCommonSchemaConfig({
+                    ...fakeSchema,
+                    aliases: { app: 'fake-db' }
+                  }),
+                data: () => getCommonDummyData(fakeData)
+              }
+            }
+          }),
+          test: async ({ fetch }) => {
+            await expect(fetch().then((r) => r.status)).resolves.toBe(200);
+
+            const db = await getDb({ name: 'fake-db' });
+
+            await expect(db.listCollections().toArray()).resolves.toStrictEqual([
+              expect.objectContaining({ name: 'fake-collection' })
+            ]);
+
+            const collection = db.collection('fake-collection');
+
+            await expect(collection.find().toArray()).resolves.toStrictEqual([
+              expect.objectContaining({ _id: expect.anything(), item: 1 }),
+              expect.objectContaining({ _id: expect.anything(), item: 2 })
+            ]);
+          }
+        });
+      },
+      { NODE_ENV: 'development', API_HYDRATE_DB: 'true', MONGODB_URI: 'ignored://' }
+    );
+  });
+});
